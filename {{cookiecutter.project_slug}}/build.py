bf23ad906a72183bcc3780f65139e4771215c00a
rossh <rossh@alleninstitute.org>
2017-08-09 15:15:29 -0700

    added correct pytest support in pyb added deployment env in pyb sphinx autobuilds in pyb
    setup entry points

--- {{cookiecutter.project_slug}}/build.py
+++ {{cookiecutter.project_slug}}/build.py
@@ -9,11 +9,13 @@ use_plugin('python.core')
 use_plugin('python.install_dependencies')
 use_plugin('python.sphinx')
 use_plugin('python.flake8')
+use_plugin('pybuilder_pytest')
 
-# TODO - this works outside of pyb but not inside it ?
-# use_plugin('python.unittest')
-
-default_task = [ 'install_build_dependencies', 'install_dependencies', 'analyze', 'run_pytest', 'publish', 'deploy']
+# pybuilder_pytest runs even without a task
+default_task = ['install_dependencies',
+                'analyze',
+                'sphinx_generate_documentation',
+                'publish']
 
 # project meta
 name = '{{ cookiecutter.project_slug }}'
@@ -24,26 +26,21 @@ authors = (Author('{{ cookiecutter.full_name }}', '{{ cookiecutter.email }}'),)
 url = '{{ cookiecutter.stash_url }}'
 
 
-@task
-def run_pytest(project, logger):
-    logger.info('Running pytest unit tests')
-    try:
-        if pytest.main([project.get_property('dir_source_pytest_python')]):
-            raise BuildFailedException('pytest: unittests failed')
-    except:
-        raise
-
-
-@task
-def deploy(project, logger):
+@task(descript='fab_deploy')
+def deploy(logger):
     logger.info('Publishing to AIBSPI')
     os.system('fab -p aibspi publish')
     os.system('fab -p aibspi publish_docs')
 
 
+@init(environments='deploy')
+def initialize_deploy(logger):
+    deploy(logger)
+
+
 @init
 def initialize(project, logger):
-    logger.info('Setting project properties.')
+    project.set_property('verbose', True)
 
     # modules / dist
     project.set_property('dir_source_main_python', '{{ cookiecutter.project_slug }}')
@@ -51,13 +48,13 @@ def initialize(project, logger):
     project.set_property('dir_dist', 'dist/{0}-{1}'.format(name, version))
 
     # testing
-    project.set_property('dir_source_pytest_python', 'tests')
+    project.set_property('dir_source_pytest_python', "tests")
 
     # documentation
     project.set_property('dir_docs', 'docs')
     project.set_property('sphinx_config_path', 'docs/')
     project.set_property('sphinx_source_dir', 'docs/')
-    project.set_property('sphinx_output_dir', 'docs/_build')
+    project.set_property('sphinx_output_dir', 'docs/_build/html')
     project.set_property('sphinx_builder', 'html')
 
     # linting
@@ -69,4 +66,6 @@ def initialize(project, logger):
     project.build_depends_on_requirements('requirements_dev.txt')
     project.depends_on_requirements('requirements.txt')
 
-
+    # entry points (typically the .py files in {{ cookiecutter.project_slug }}
+    project.set_property('distutils_entry_points',
+                         {'console_scripts': ['{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}:main']})
