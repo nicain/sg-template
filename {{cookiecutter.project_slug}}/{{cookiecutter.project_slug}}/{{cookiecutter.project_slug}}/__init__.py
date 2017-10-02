f724543056d2f1c4be56e04a46966bf4c8bd3877
Ross <r.hytnen@gmail.com>
2017-10-02 14:58:39 -0700

    Changing the return type from dict to named tuple.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import os
-import logging.config
-import yaml
 import datetime
+import logging.config
+import os
 import shutil
 import sys
+from collections import namedtuple
 
-__author__ = """{{ cookiecutter.full_name }}"""
-__email__ = '{{ cookiecutter.email }}'
-__version__ = '{{ cookiecutter.version }}'
+import yaml
+
+__author__ = """Ross Hytnen"""
+__email__ = 'rossh@alleninstitute.org'
+__version__ = '0.1.0'
 
 
-def init_log(log_config_file=None):
+def init_log(log_config_file=None, override_local=False):
     """
     Standard log initialization boiler plate that attempts to find a configuration file
     from the log_config_file variable, then the LOG_CONFIG_FILE environment variable. If
@@ -26,7 +28,7 @@ def init_log(log_config_file=None):
     if log_config_file is None:
         log_config_file = 'log_config_v1.yml'
 
-    config = source_project_configuration(log_config_file)
+    config = source_project_configuration(log_config_file, override_local=override_local, as_dict=True)
     if config is None:
         logging.basicConfig(level=logging.DEBUG)
         logging.warning(f'could not find {log_config_file}.  Using default configuration.')
@@ -58,7 +60,7 @@ def cache_remote_config(configuration_file):
     :return:
     """
     local_path = f'{__path__[0]}/resources/{configuration_file}'
-    remote_path = f'{os.getenv("MPE_CONFIGURATION_PATH", "//allen/aibs/mpe/Rigs/configuration")}/{configuration_file}'
+    remote_path =  f'{os.getenv("MPE_CONFIGURATION_PATH", "//allen/aibs/mpe/Rigs/configuration")}/{configuration_file}'
 
     logging.info('caching remote configuration')
     if os.path.exists(local_path):
@@ -70,7 +72,7 @@ def cache_remote_config(configuration_file):
     shutil.copyfile(remote_path, local_path)
 
 
-def source_project_configuration(configuration_file, override_local=True):
+def source_project_configuration(configuration_file, override_local=True, as_dict=False):
     """
     Find a project configuration file on the network, compare it to the local configuration and cache it.
     :param configuration_file:  Name of the configuration file
@@ -79,7 +81,7 @@ def source_project_configuration(configuration_file, override_local=True):
     :return: A dict corresponding to the programs configuration file
     """
     local_path = f'{__path__[0]}/resources/{configuration_file}'
-    remote_path = f'{os.getenv("MPE_CONFIGURATION_PATH", "//allen/aibs/mpe/Rigs/configuration")}/{configuration_file}'
+    remote_path =  f'{os.getenv("MPE_CONFIGURATION_PATH", "//allen/aibs/mpe/Rigs/configuration")}/{configuration_file}'
 
     local_config = parse_config(local_path)
     remote_config = parse_config(remote_path)
@@ -88,15 +90,24 @@ def source_project_configuration(configuration_file, override_local=True):
         raise FileNotFoundError('no valid configurations found.')
 
     if not remote_config or not override_local:
-        return local_config
+        logging.info(f'using local configuration: {local_path}')
+        return dict_to_namedtuple(local_config) if not as_dict else local_config
 
+    logging.info(f'using remote configuration: {remote_path}')
     if not local_config:
         cache_remote_config(configuration_file)
-        return remote_config
+        return dict_to_namedtuple(remote_config) if not as_dict else remote_config
 
     local_modify_time = os.path.getmtime(local_path)
     remote_modify_time = os.path.getmtime(remote_path)
     if remote_modify_time > local_modify_time:
         cache_remote_config(configuration_file)
 
-    return remote_config
+    return dict_to_namedtuple(remote_config) if not as_dict else remote_config
+
+
+def dict_to_namedtuple(dictionary):
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            dictionary[key] = dict_to_namedtuple(value)
+    return namedtuple('dotDict', dictionary.keys())(**dictionary)
