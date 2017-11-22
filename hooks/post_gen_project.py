4a03e879cc3f1ed6a37cc641c576f978ceb37f17
nicholasc <nicholasc@alleninstitute.org>
2017-11-22 04:25:07 +0000

    this will generate the yaml file of inputs from the user
    updated .gitignore
    
    changed yaml target: cookiecutter.yaml to .cookiecutter.yaml

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 import os
+import json
+import ruamel.yaml as yaml
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
-
 if __name__ == '__main__':
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
+
+    # Read the json file generated from the user input:
+    config_file_name_read = os.path.join(PROJECT_DIRECTORY, '.cookiecutter.json')
+    cookiecutter_json = json.load(open(config_file_name_read, 'r'))
+
+    # Pop off the _template; this was not input by the user
+    cookiecutter_json.pop('_template')
+    
+    # Write back out to a yaml file compatible with --config-file command line option
+    config_file_name_write = os.path.join(PROJECT_DIRECTORY, '.cookiecutter.yaml')
+    yaml.safe_dump({'default_context':cookiecutter_json}, open(config_file_name_write, 'w'), default_flow_style=False)
