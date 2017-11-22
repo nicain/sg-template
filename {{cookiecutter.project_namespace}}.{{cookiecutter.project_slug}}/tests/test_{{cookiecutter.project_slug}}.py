6c1191e2a939cc4fabfff063f53b8f402aeabfb2
nicholasc <nicholasc@alleninstitute.org>
2017-11-22 03:07:03 +0000

    added test file

---
+++ {{cookiecutter.project_namespace}}.{{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_ephys_pink_noise
+----------------------------------
+
+Tests for `ephys_pink_noise` module.
+"""
+import pytest
+
+
+@pytest.fixture
+def decorated_example():
+    """Sample pytest fixture.
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+
+def test_example(decorated_example):
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    import {{cookiecutter.project_namespace}}.{{cookiecutter.project_slug}}
+    from {{cookiecutter.project_namespace}} import {{cookiecutter.project_slug}}
+
+
+
+
