53d5005f3afc16feb8289ac02ffd478739e51430
nicain <nicain.seattle@gmail.com>
2017-12-20 19:34:49 -0800

    30 (#55)
    * moved splash generation out of script and into post-hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,6 +5,8 @@ import json
 import ruamel.yaml as yaml
 import textwrap
 from subprocess import call
+import textwrap
+from io import StringIO
 
 MAX_WIDTH = 4096
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
@@ -12,6 +14,39 @@ COOKIECUTTER_CONFIG_DIR = os.path.join(PROJECT_DIRECTORY, '.cookiecutter')
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+def get_splash_text():
+    
+    buf = StringIO()
+
+    buf.write(unicode("="*80+"\n"))
+
+    txt = "Thanks for using the AIBS cookiecutter python project template."
+    buf.write(unicode( "\n".join(textwrap.wrap(txt, width=80)) +"\n"))
+
+    buf.write(unicode("\nLicense:"+"\n"))
+    txt = "Your project is currently unlicensed (i.e. does not have a LICENSE file in the repository). If you would like to publicly release this code, you will need to submit your project to Innovation Central (http://ai/Legal/Innovation/SitePages/Home.aspx).  For more information on code release policies and procedures, check out http://confluence.corp.alleninstitute.org/display/PP/Github+FAQ"
+    buf.write(unicode( "\n".join(textwrap.wrap(txt, width=80)) +"\n"))
+
+    buf.write(unicode("\nContributing:"+"\n"))
+    txt = "Your public facing project should have a statement about what level of support users should expect. Here are 4 example suggestions about a level of support notification you could include on your github repo; by default the first example (no expectation of support) is included in README.md, but feel free to customize:"
+    buf.write(unicode( "\n".join(textwrap.wrap(txt, width=80, replace_whitespace=False)) +"\n"))
+
+    txt = """
+        1. We are not currently supporting this code, but simply releasing it to the community AS IS but are not able to provide any guarantees of support.  The community is welcome to submit issues, but you should not expect an active response.
+
+        2. We are planning on occasional updating this tool with no fixed schedule. Community involvement is encouraged through both issues and pull requests.
+
+        3. This code is an important part of the internal Allen Institute code base and we are actively using and maintaining it. Issues are encouraged, but because this tool is so central to our mission pull requests might not be accepted if they conflict with our existing plans.
+
+        4. We are releasing this code to the public as a tool we expect others to use. Issues are welcomed and we expect to address them promptly, pull requests will vetted by our staff before inclusion.
+    """
+
+    buf.write(unicode( "\n".join(textwrap.wrap(txt, width=80, replace_whitespace=False, initial_indent='    ', subsequent_indent='    ')) +"\n"))
+
+    buf.write(unicode("="*80))
+
+    return str(buf.getvalue())
 
 if __name__ == '__main__':
 
@@ -69,6 +104,4 @@ if __name__ == '__main__':
         shutil.rmtree( 'to_namespace' )
         shutil.rmtree( 'to_slug' )
 
-    splash_file_name = os.path.join(PROJECT_DIRECTORY, 'docs', 'splash.txt')
-    with open(splash_file_name, 'r') as fin:
-        print fin.read()
+    print(get_splash_text())
