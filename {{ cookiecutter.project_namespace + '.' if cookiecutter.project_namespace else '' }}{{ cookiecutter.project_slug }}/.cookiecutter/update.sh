a3b863561a3f50a5f7470010355b4c1d4f526d89
nicain <nicain.seattle@gmail.com>
2018-01-18 10:51:29 -0800

    Update update.sh

--- {{ cookiecutter.project_namespace + '.' if cookiecutter.project_namespace else '' }}{{ cookiecutter.project_slug }}/.cookiecutter/update.sh
+++ {{ cookiecutter.project_namespace + '.' if cookiecutter.project_namespace else '' }}{{ cookiecutter.project_slug }}/.cookiecutter/update.sh
@@ -8,7 +8,7 @@ python update_from_repo.py
 # Add post-cookiecutter commands that you always want run here:
 git checkout -- $PROJECT_DIR/README.md
 git checkout -- $PROJECT_DIR/AUTHORS.rst
-git checkout -- $PROJECT_DIR/requirements.txt
+git checkout -- $PROJECT_DIR/Pipfile
 git checkout -- $PROJECT_DIR/.cookiecutter/update.sh
 
 # Enter patch mode on remaining diffs:
