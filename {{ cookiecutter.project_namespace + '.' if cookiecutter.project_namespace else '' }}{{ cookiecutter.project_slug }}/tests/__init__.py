872a916c75ae9a947e4f6c86a4e57d9ab79c9559
Jed Perkins <perkins.jed@gmail.com>
2018-02-09 13:29:14 -0800

    Fix entrypoint test on windows (#85)

--- {{ cookiecutter.project_namespace + '.' if cookiecutter.project_namespace else '' }}{{ cookiecutter.project_slug }}/tests/__init__.py
+++ {{ cookiecutter.project_namespace + '.' if cookiecutter.project_namespace else '' }}{{ cookiecutter.project_slug }}/tests/__init__.py
@@ -4,5 +4,7 @@ import os
 import sys
 
 def entrypoint_exists(entry_point):
+    if sys.platform == "win32":
+        entry_point += ".exe"
     executable_dir = os.path.dirname(sys.executable)
     return os.path.exists(os.path.join(executable_dir, entry_point))
